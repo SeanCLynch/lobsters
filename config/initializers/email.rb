fe96b988af3d7aad33aa51771ba045a203099ecf
Sean Lynch <sean.lynch@thrivecommerce.com>
2019-02-19 12:31:28 -0500

    fixed env variable access

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  :address => ENV.fetch("MAILGUN_SMTP_SERVER", "127.0.0.1"),
-  :port => Integer(ENV.fetch("MAILGUN_SMTP_PORT", 25)),
+  :address => ENV["MAILGUN_SMTP_SERVER"],
+  :port => Integer(ENV["MAILGUN_SMTP_PORT"]),
   :domain => Rails.application.domain,
   :enable_starttls_auto => (ENV["SMTP_STARTTLS_AUTO"] == "true"),
-  :user_name => ENV.fetch("MAILGUN_SMTP_LOGIN", ""),
-  :password => ENV.fetch("MAILGUN_SMTP_PASSWORD", ""),
+  :user_name => ENV["MAILGUN_SMTP_LOGIN"],
+  :password => ENV["MAILGUN_SMTP_PASSWORD"],
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
