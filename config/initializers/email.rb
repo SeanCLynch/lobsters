336ac515c7ad5099ec2c0dc94fa03bf0ceb1f475
Sean Lynch <sean.lynch@thrivecommerce.com>
2019-02-19 12:11:44 -0500

    added mailgun config

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-  :address => ENV.fetch("SMTP_HOST", "127.0.0.1"),
-  :port => Integer(ENV.fetch("SMTP_PORT", 25)),
+  :address => ENV.fetch("MAILGUN_SMTP_SERVER", "127.0.0.1"),
+  :port => Integer(ENV.fetch("MAILGUN_SMTP_PORT", 25)),
   :domain => Rails.application.domain,
   :enable_starttls_auto => (ENV["SMTP_STARTTLS_AUTO"] == "true"),
-  :user_name => ENV.fetch("SMTP_USERNAME", ""),
-  :password => ENV.fetch("SMTP_PASSWORD", ""),
+  :user_name => ENV.fetch("MAILGUN_SMTP_LOGIN", ""),
+  :password => ENV.fetch("MAILGUN_SMTP_PASSWORD", ""),
 }
